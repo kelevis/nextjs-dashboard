8a804256dd94d1699e4a01d99872a37cfa4303d9
tree <lushuwu@chinamobile.com>
2024-03-29 10:47:38 +0800

    fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,25 +8,20 @@ import { redirect } from 'next/navigation';   //重定向回到invoices页面
 const FormSchema = z.object({
     id: z.string(),
 
-    customerId: z.string({invalid_type_error: 'Please select a customer.',
-        }),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
 
-    amount:
-        z.coerce.number()
+    amount: z.coerce
+        .number()
         .gt(0, { message: 'Please enter an amount greater than $0.' }),
 
-    status: z.enum(['pending', 'paid'],{invalid_type_error: 'Please select an invoice status.',}),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
+
     date: z.string(),
 });
-
-
-// const FormSchema = z.object({
-//     id: z.string(),
-//     customerId: z.string(),
-//     amount: z.coerce.number(),
-//     status: z.enum(['pending', 'paid']),
-//     date: z.string(),
-// });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 
@@ -39,52 +34,43 @@ export type State = {
     message?: string | null;
 };
 
-export async function createInvoice(prevState: State, formData: FormData) {
-    // // Validate form using Zod
-    // const validatedFields = CreateInvoice.safeParse({
-    //     customerId: formData.get('customerId'),
-    //     amount: formData.get('amount'),
-    //     status: formData.get('status'),
-    // });
-    //
-    // // If form validation fails, return errors early. Otherwise, continue.
-    // if (!validatedFields.success) {
-    //     return {
-    //         errors: validatedFields.error.flatten().fieldErrors,
-    //         message: 'Missing Fields. Failed to Create Invoice.',
-    //     };
-    // }
-
-    // Prepare data for insertion into the database
-    // const { customerId, amount, status } = validatedFields.data;
-    // const amountInCents = amount * 100;
-    // const date = new Date().toISOString().split('T')[0];
-
+export async function createInvoice(formData: FormData) {
+    // const { customerId, amount, status } = CreateInvoice.parse({
+    // const rawFormData = {
     const {customerId, amount, status} = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-
+    // Test it out:
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    // Insert data into the database
+    // console.log(rawFormData);
+    // console.log("typeof rawFormData.amount:",typeof rawFormData.amount);
+
     try {
         await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-    } catch (error) {
-        // If a database error occurs, return a more specific error.
-        return {
-            message: 'Database Error: Failed to Create Invoice.',
-        };
+
+
+    }catch (error){
+        console.log("insert Data, customerId, amountInCents, status, date:", customerId, amountInCents, status, date);
+        return { message: 'Database Error: Failed to crate Invoice.' };
+
     }
 
-    // Revalidate the cache for the invoices page and redirect the user.
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+
+
+
+
+
+
+
 }
 
 // Use Zod to update the expected types
